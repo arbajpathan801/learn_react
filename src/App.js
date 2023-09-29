722e3ef8390c4ff6aeb067226b61747db4933745
ArbajPathan <arbajpatel0432>
2023-09-29 23:11:04 +0530

    done with expensedaitels

--- src/App.js
+++ src/App.js
@@ -6,21 +6,21 @@ function App() {
   const expenses = [
     {
       id: 'e1',
-      date: new Date(2023, 8, 28),
+      date: new Date(2023, 8, 12),
       title: 'Car Insurance',
       LocationofExpenditure: 'Mumbai',
       amount: 270
     },
     {
       id: 'e2',
-      date: new Date(2023, 3, 28),
+      date: new Date(2023, 3, 24),
       title: 'Mobile Recharge',
       LocationofExpenditure: 'Goa',
       amount: 90
     },
     {
       id: 'e3',
-      date: new Date(2023, 0, 28),
+      date: new Date(2023, 0, 6),
       title: 'Tv Bill',
       LocationofExpenditure: 'Village',
       amount: 98
@@ -34,35 +34,35 @@ function App() {
     },
     {
       id: 'e5',
-      date: new Date(2023, 8, 28),
+      date: new Date(2023, 8, 5),
       title: 'New Phone',
       LocationofExpenditure: 'Dubai',
       amount: 3500
     },
     {
       id: 'e6',
-      date: new Date(2023, 8, 28),
+      date: new Date(2023, 8, 9),
       title: 'Driver',
       LocationofExpenditure: 'Hydrabad',
       amount: 1000
     },
     {
       id: 'e7',
-      date: new Date(2023, 5, 28),
+      date: new Date(2023, 5, 10),
       title: 'Milk',
       LocationofExpenditure: 'Chenai',
       amount: 20
     },
     {
       id: 'e8',
-      date: new Date(2023, 6, 28),
+      date: new Date(2023, 6, 13),
       title: 'House Rent',
       LocationofExpenditure: 'Pune',
       amount: 520
     },
     {
       id: 'e9',
-      date: new Date(2023, 8, 28),
+      date: new Date(2023, 8, 25),
       title: 'Bike Insurance',
       LocationofExpenditure: 'Delhi',
       amount: 252
@@ -79,7 +79,7 @@ function App() {
           title={expense.title}
           amount={expense.amount}
           LocationofExpenditure={expense.LocationofExpenditure}
-        ></ExpenseItem>
+        />
       )
       )}
     </div>
